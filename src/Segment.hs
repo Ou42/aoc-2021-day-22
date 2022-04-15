f65aab55290b19a7d55fd5f542646075e3c33d40
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-04-15 08:59:44 -0700

    Added ResultType

--- src/Segment.hs
+++ src/Segment.hs
@@ -8,9 +8,29 @@ import Utils (readInt, toTuple)
 
 newtype Segment = Segment (Int, Int) deriving (Eq) -- fst <= snd
 
+{- | The catagories of results from combining two Segments together
+   |
+-}
+data ResultType
+  = NotIntersecting
+  | Adjacent
+  | Skinned Int -- They are combined in single cube vector (the skin)
+  | Intersects Segment
+  | Encompasses [Segment] -- 2 segments, 1st is left difference; 2nd right difference.  Lined up edges are an issue.
+  | EncompassedBy [Segment] -- Identical to previous; segments are reverse order
+  | Same     -- Segments are identical
+
 instance Show Segment where
    show (Segment s) = "(" ++ show (fst s) ++ "," ++ show (snd s) ++ ")"
 
 {- "-36..17" -}
 toSegment :: T.Text -> Segment
 toSegment pairStr = Segment $ toTuple $ map readInt $ T.splitOn ".." pairStr
+
+augment :: Segment -> Segment -> ResultType
+augment s1 s2 =
+  undefined
+
+reduce :: Segment -> Segment -> ResultType
+reduce s1 s2 =
+  undefined

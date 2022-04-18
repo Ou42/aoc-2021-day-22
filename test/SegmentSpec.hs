8ae25788c75aca64a6525c8d00f86063f0efe989
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-04-18 12:40:14 -0700

    Added functionality and tests for target adjacent left

--- test/SegmentSpec.hs
+++ test/SegmentSpec.hs
@@ -2,12 +2,24 @@ module SegmentSpec where
 
 import Test.Hspec
 
-import Segment (ResultType(..), Segment(..), Source(..), Target(..), TargetAdjacentRight(..), compareSegments)
+import Segment ( ResultType(..)
+               , Segment(..)
+               , Source(..)
+               , Target(..)
+               , TargetAdjacentLeft(..)
+               , TargetAdjacentRight(..)
+               , compareSegments
+               )
 
 segmentSpec =
   describe "compareSegments" $ do
     it "detects no overlap" $ do
       compareSegments (Source (5,6))  (Target (7,8)) `shouldBe` NoOverlap
     it "detects target is right-adjacent to source" $ do
-      compareSegments (Source (5,10)) (Target (8,15)) `shouldBe` OverlapsTargetLeft (TargetAdjacentRight (Target (11, 15)))
-
+      compareSegments (Source (5,10)) (Target (8,15))
+        `shouldBe`
+          OverlapsTargetLeft (TargetAdjacentRight (Target (11, 15)))
+    it "detects target is left-adjacent to source" $ do
+      compareSegments (Source (8, 15)) (Target (5, 10))
+        `shouldBe`
+          OverlapsTargetRight (TargetAdjacentLeft (Target (5, 7)))

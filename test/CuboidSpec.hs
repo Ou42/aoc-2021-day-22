0858e1c7a4b2eeff0d01b711b7ef3c46b09077be
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-04-18 18:15:43 -0700

    All cuboid and segment types are named differently based upon whether they are source or target

--- test/CuboidSpec.hs
+++ test/CuboidSpec.hs
@@ -1,10 +1,6 @@
-{- HLINT ignore -}
-
 module CuboidSpec where
 
 import Test.Hspec
 
 import Cuboid (Target(..))
 import Segment (Segment(..))
-
--- import SegmentTest (areSame, targetIsAdjacentLeft, targetIsAdjacentRight)

88bf1dadf836ebf33e0e5f2a03afe7fb8e0ffc5f
oldfartdeveloper <scottnelsonsmith@gmail.com>
2022-04-28 14:28:59 -0700

    MILESTONE REACHED! Part A solved

--- src/SolutionChoices.hs
+++ src/SolutionChoices.hs
@@ -4,10 +4,11 @@ module SolutionChoices where
 
 import Test.Hspec
 
-import Cuboid (volume)
+import Cuboid (volume, Source(..), Target(..))
 import RebootStep (generateRemnant, parseInputText)
-import Remnant (Remnant)
+import Remnant (Remnant, emptyRemnant, mkAxisResults)
 import qualified RunningCode as RC
+import Segment (AdjLeft(..), AdjRight(..), AxisResult(..), Overlap(..), SrcSeg(..), TrgSeg(..))
 
 inputFilePath :: String
 inputFilePath = "data/Day-22-INPUT.txt"
@@ -97,7 +98,52 @@ reduceToTotalVolume :: Remnant -> Int
 reduceToTotalVolume = sum . map volume
 
 calculatePartA :: Remnant -> Remnant
-calculatePartA = id
+calculatePartA = foldl truncateTarget emptyRemnant
+
+truncateTarget :: Remnant -> Target -> Remnant
+truncateTarget outgoingRemnant target =
+  let
+    axisResults = mkAxisResults region50Source target
+  in
+    if NoOverlap `elem` axisResults then
+      outgoingRemnant
+    else
+      let
+         (outgoingTarget, _) = foldl sourceTargetIntersection (target, 0) axisResults
+      in
+      outgoingTarget : outgoingRemnant
+
+{- | generate the overlap cuboid from the compare -}
+sourceTargetIntersection :: (Target, Int) -> AxisResult -> (Target, Int)
+sourceTargetIntersection (target, axisOffset) axisResult =
+   case axisResult of
+      Overlaps ->
+         (target, axisOffset + 1)
+      OverlapsLeft (Overlap overlap) (AdjRight _) ->
+         createCommon overlap
+      OverlapsRight (AdjLeft _) (Overlap overlap) ->
+         createCommon overlap
+      OverlappedByTarget (AdjLeft _) (Overlap overlap) (AdjRight _) ->
+         createCommon overlap
+   where
+      createCommon :: TrgSeg -> (Target, Int)
+      createCommon overlap' =
+         ( createPiece overlap'
+         , axisOffset + 1
+         )
+
+      {- | Return a torn-off piece of the original cuboid -}
+      createPiece :: TrgSeg -> Target
+      createPiece segment =
+        let
+          Target incoming = target
+        in
+        Target $ take axisOffset incoming <> [segment] <> drop (axisOffset+1) incoming
+
+region50 :: SrcSeg
+region50 = SrcSeg (-50, 50)
+
+region50Source = Source [region50, region50, region50]
 
 specWith :: String -> Spec
 specWith inputText = do
